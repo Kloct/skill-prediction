fa6231cbe6efeb70a4a9fd590606379f7a34f02b
unknown <zelekie@protonmail.com>
2022-02-26 11:10:38 -0300

    oop...

--- config/skills.js
+++ config/skills.js
@@ -1490,30 +1490,13 @@ module.exports = {
 				moveDir: 1
 			}
 		},
-		'*': {
+		8: { // Rapid Fire
+			'*': {
 				noInterrupt: [6, '8-2'], // idk why this is here
 				triggerAbnormal: { 600200: 7000 },
 			},
 			0: { length: [200, 300] },
-			2: { length: [160, 160, 280] },
-		8: { // Rapid Fire
-			'*': { noRetry: true },
-			0: { noInterrupt: [6] },
-			1: {
-				level: {
-					5: { noInterrupt: [6] }
-				}
-			},
-			2: true,
-			3: true,
-			4: true,
-			5: true,
-			6: true,
-			7: true,
-			11: true,
-			12: true,
-			13: true,
-			14: true
+			2: { length: [160, 160, 280] }
 		},
 		9: { // Slow Trap
 			0: true
